e5310689a5f64b01fda14925291567da05dfd5ee
Greg Morgan <gregm9999@gmail.com>
2018-10-26 01:26:32 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,11 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-if position_taken? && between (0-8)
+if position_taken? && index.between (0-8)
   return true
 else
   return false
-end
+
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
