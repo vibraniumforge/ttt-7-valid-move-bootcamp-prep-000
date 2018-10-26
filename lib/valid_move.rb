49385abaf9adeac765b22d7e805868e40b874352
Greg Morgan <gregm9999@gmail.com>
2018-10-26 02:15:58 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ def valid_move?(board, index)
 if position_taken?(board, index)==true && index>=0 && index<=8
   return true
 elsif position_taken?(board, index)==false
-  return "1"
+  return false
 end
 end
 
